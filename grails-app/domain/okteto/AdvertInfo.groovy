6a098635b76697a5e6922518769fa5f95c580605
sneira <sofianeirag@gmail.com>
2021-09-28 22:44:52 +0200

    Load CSV data on app start

---
+++ grails-app/domain/okteto/AdvertInfo.groovy
@@ -0,0 +1,9 @@
+package okteto
+
+class AdvertInfo {
+    String datasource
+    String campaign
+    Date day
+    Integer clicks
+    Long impressions
+}
--- grails-app/init/okteto/BootStrap.groovy
+++ grails-app/init/okteto/BootStrap.groovy
@@ -1,8 +1,29 @@
 package okteto
 
+import org.hibernate.SessionFactory
+import org.springframework.core.io.Resource
+
+import java.text.SimpleDateFormat
+
 class BootStrap {
 
+    def assetResourceLocator
+    SessionFactory sessionFactory
+
     def init = { servletContext ->
+        Resource originalData = assetResourceLocator.findAssetForURI('original-data.csv')
+        originalData.inputStream.toCsvReader([skipLines:1]).eachLine { tokens ->
+            AdvertInfo.withNewTransaction {
+                AdvertInfo adInfo = new AdvertInfo(datasource: tokens[0],
+                        campaign: tokens[1],
+                        day: new SimpleDateFormat('MM/dd/yy').parse(tokens[2]),
+                        clicks: tokens[3],
+                        impressions: tokens[4])
+                adInfo.save(failOnError: true, flush: true)
+            }
+            sessionFactory.currentSession.clear()
+        }
+        println "Number of rows: ${AdvertInfo.count()}"
     }
     def destroy = {
     }
